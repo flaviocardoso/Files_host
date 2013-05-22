64c6f4a1a8e2809193dc4588137dc5107cd7a5d1
flaviocardoso <flavioc401@gmail.com>
2013-05-22 18:41:27 -0300

    Update flame2.js

--- flame2.js
+++ flame2.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-  $("body").append("<iframe id="frame1></iframe><iframe id="frame2"></iframe><iframe id="frame3"></iframe><iframe id="frame4></iframe>");
+  $("body").append("<iframe id="frame1"></iframe><iframe id="frame2"></iframe><iframe id="frame3"></iframe><iframe id="frame4"></iframe>");
 });  
